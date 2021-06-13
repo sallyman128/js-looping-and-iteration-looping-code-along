683af9bc245534dfca7f00460076cc08b49aab91
Salmaan <salmaan.arif.ali@gmail.com>
2021-06-13 14:22:58 -0400

    commit tree 72dfe7f9e2518eda9df68a34a7870d5e0fa81a4a

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  let arr = [];
+  for (let i=0; i < names.length; i++) {
+    arr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return arr;
+}
+
+function countDown(number) {
+  let i = number;
+  while (i >= 0) {
+    console.log(i--);
+  }
+}
